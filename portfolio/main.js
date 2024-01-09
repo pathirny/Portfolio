864fc0c657aef67e3012c4ff7e1707405817f924
Patryk Hirny <pathirny@googlemail.com>
2024-01-09 10:42:11 +0000

    created vite project

---
+++ portfolio/main.js
@@ -0,0 +1,43 @@
+var form = document.getElementById("form");
+
+// Handle submit function handles the form submission
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form";
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+    });
+}
+form.addEventListener("submit", handleSubmit);
+
+// typewriter effect
+
+const typewriter = new Typewriter("#typewriter", {
+  strings: ["Hello", "World"],
+  autoStart: true,
+});
+
+console.log(typewriter);
